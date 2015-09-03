4105204e583cf4a72d6c6430229d31276488b03b
David Romaní <david@flux.cat>
2015-09-03 13:08:50 +0200

    add BS

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // vendors
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            // app
             new AppBundle\AppBundle(),
         );
 
