a6e1d601bec9b1a95b7339d9f443c660637bbb65
Anton Serra <aserratorta@gmail.com>
2016-04-10 07:20:47 +0200

    PostAdmin-improve action buttons

--- src/AppBundle/Admin/PostAdmin.php
+++ src/AppBundle/Admin/PostAdmin.php
@@ -219,7 +219,7 @@ class PostAdmin extends AbstractBaseAdmin
                 'title',
                 null,
                 array(
-                    'label'    => 'backend.admin.title',
+                    'label'    => 'backend.admin.post.title',
                     'editable' => true,
                 )
             )
@@ -244,9 +244,11 @@ class PostAdmin extends AbstractBaseAdmin
                 'actions',
                 array(
                     'actions' => array(
-                        'show'   => array(),
-                        'edit'   => array(),
-                        'delete' => array(),
+                        'show'   => array(
+                            'template' => '::Admin/Buttons/list__action_show_button.html.twig'
+                        ),
+                        'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
                     ),
                     'label'   => 'backend.admin.actions',
                 )
