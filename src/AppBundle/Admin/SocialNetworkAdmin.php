33157d1185ff9b7c9f2d904cef0cb12eb2b5d3c8
Anton Serra <aserratorta@gmail.com>
2016-04-15 22:19:23 +0200

    add SocialNetworkAdmin

---
+++ src/AppBundle/Admin/SocialNetworkAdmin.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+/**
+ * Class SocialNetworkAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class SocialNetworkAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Social Network';
+    protected $baseRoutePattern = 'coworkers/social-network';
+    protected $datagridValues = array(
+        '_sort_by'    => 'url',
+        '_sort_order' => 'asc',
+    );
+
+    /**
+     * Configure route collection
+     *
+     * @param RouteCollection $collection
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection
+            ->remove('show')
+            ->remove('batch');
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('backend.admin.general', $this->getFormMdSuccessBoxArray(7))
+            ->add(
+                'url',
+                null,
+                array(
+                    'label' => 'backend.admin.social_network.url',
+                )
+            )
+            ->add(
+                'category',
+                null,
+                array(
+                    'label' => 'backend.admin.social_network.category',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'coworker',
+                null,
+                array(
+                    'label' => 'backend.admin.social_network.coworker',
+                    'editable' => true,
+                )
+            )
+            ->end()
+            ->with('backend.admin.controls', $this->getFormMdSuccessBoxArray(5))
+            ->add(
+                'enabled',
+                'checkbox',
+                array(
+                    'label'    => 'backend.admin.enabled',
+                    'required' => false,
+                )
+            )
+            ->end();
+    }
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'url',
+                null,
+                array(
+                    'label' => 'backend.admin.social_network.url',
+                )
+            )
+            ->add(
+                'category',
+                null,
+                array(
+                    'label' => 'backend.admin.social_network.category',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'coworker',
+                null,
+                array(
+                    'label' => 'backend.admin.social_network.coworker',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label' => 'backend.admin.enabled',
+                    'editable' => true,
+                )
+            );
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'url',
+                null,
+                array(
+                    'label' => 'backend.admin.social_network.url',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'category',
+                null,
+                array(
+                    'label' => 'backend.admin.social_network.category',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label' => 'backend.admin.enabled',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label'   => 'backend.admin.actions',
+                    'actions' => array(
+                        'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
+                    )
+                )
+            );
+    }
+}
