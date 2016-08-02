8f7a0b92889813960bf8bb01c20fe1fc9ad0264a
Wil Iglesias <wiglesias83@gmail.com>
2016-08-02 18:38:13 +0200

    show flash message

--- src/AppBundle/Controller/Front/DefaultController.php
+++ src/AppBundle/Controller/Front/DefaultController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\ContactMessage;
 use AppBundle\Form\Type\ContactMessageType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
@@ -23,12 +24,22 @@ class DefaultController extends Controller
     /**
      * @Route("/contacte", name="front_contact")
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
         $contactMessage = new ContactMessage();
         $form = $this->createForm(ContactMessageType::class, $contactMessage);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash(
+                'notice',
+                'El teu missatge s\'ha enviat correctament'
+            );
+        }
 
         return $this->render(':Frontend:contact.html.twig', array(
             'formContact' => $form->createView(),
