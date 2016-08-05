5d74b4e187a9a35b69e62e80c03b2f870a1730d2
Anton Serra <aserratorta@gmail.com>
2016-08-05 23:32:48 +0200

    add SitemapListerner

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
+            new Presta\SitemapBundle\PrestaSitemapBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
             // App
