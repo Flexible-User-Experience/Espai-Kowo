d19bcb8f9ad37473e1624ba2da8dc2b49fecb1fc
David Romaní <david@flux.cat>
2016-10-13 20:28:14 +0200

    fix final delivery problem

--- src/AppBundle/Service/NotificationService.php
+++ src/AppBundle/Service/NotificationService.php
@@ -85,7 +85,7 @@ class NotificationService
     {
         $this->messenger->sendEmail(
             $this->amd,
-            $contactMessage->getEmail(),
+            $this->amd,
             'Missatge de prova-ho gratis pàgina web ' . $this->urlBase,
             $this->twig->render(':Mails:free_trial_admin_notification.html.twig', array(
                 'contact' => $contactMessage,
@@ -101,7 +101,7 @@ class NotificationService
     public function sendContactAdminNotification(ContactMessage $contactMessage)
     {
         $this->messenger->sendEmail(
-            $contactMessage->getEmail(),
+            $this->amd,
             $this->amd,
             'Missatge de contacte pàgina web ' . $this->urlBase,
             $this->twig->render(':Mails:contact_form_admin_notification.html.twig', array(
@@ -118,7 +118,7 @@ class NotificationService
     public function sendNewsletterSubscriptionAdminNotification(ContactMessage $contactMessage)
     {
         $this->messenger->sendEmail(
-            $contactMessage->getEmail(),
+            $this->amd,
             $this->amd,
             'Missatge de newsletter pàgina web ' . $this->urlBase,
             $this->twig->render(':Mails:newsletter_form_admin_notification.html.twig', array(
