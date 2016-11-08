1990333fc861e82cc670ad7858ada3ae284d24ab
Anton Serra <aserratorta@gmail.com>
2016-11-08 23:15:10 +0100

    Evaluate contact name lenght

--- src/AppBundle/Manager/MailchimpManager.php
+++ src/AppBundle/Manager/MailchimpManager.php
@@ -55,30 +55,23 @@ class MailchimpManager
      */
     public function subscribeContactToList(ContactMessage $contact, $listId)
     {
-        $result = $this->mailChimp->post('lists/' . $listId . '/members', array(
-            'email_address' => $contact->getEmail(),
-            'status'        => 'subscribed'
-        ));
-
-//        $list = $this->mailChimp->getList();
-//        //Evaluate contact name
-//        $explodeName = explode(" ", $contact->getName());
-//        if(count($explodeName) === 1){
-//            $list->setMerge(array(
-//                'FNAME' => $explodeName[0]
-//                )
-//            );
-//        } else if (count($explodeName) >= 2) {
-//            $list->setMerge(array(
-//                'FNAME' => $explodeName[0],
-//                'LNAME' => $explodeName[1]
-//                )
-//            );
-//        }
-//        $list->setDoubleOptin(false);
-//        $result = $list->Subscribe($contact->getEmail());
-        // Check contact to list
-        if ($result == false) {
+        //Evaluate contact name and subscribe
+        $explodeName = explode(" ", $contact->getName());
+        if(count($explodeName) === 1){
+            $result = $this->mailChimp->post('lists/' . $listId . '/members', array(
+                'email_address' => $contact->getEmail(),
+                'status'        => 'subscribed',
+                'merge_fields'  => ['FNAME' => $explodeName[0]]
+            ));
+        } else if (count($explodeName) >= 2) {
+            $result = $this->mailChimp->post('lists/' . $listId . '/members', array(
+                'email_address' => $contact->getEmail(),
+                'status'        => 'subscribed',
+                'merge_fields'  => ['FNAME' => $explodeName[0], 'LNAME'=> $explodeName[1]]
+            ));
+        }
+         //Check contact to list
+        if ($result === false) {
             $this->messenger->sendCommonAdminNotification('En ' . $contact->getEmail() . ' no s\'ha pogut registrar a la llista de Mailchimp');
         }
 
