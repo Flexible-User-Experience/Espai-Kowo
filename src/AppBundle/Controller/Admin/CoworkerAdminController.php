43334b3a96d7f77fcfb50f532899c9147d3bbcb6
Anton Serra <aserratorta@gmail.com>
2017-02-02 17:31:28 +0100

    implement logic dataAction in coworker admin controller

--- src/AppBundle/Controller/Admin/CoworkerAdminController.php
+++ src/AppBundle/Controller/Admin/CoworkerAdminController.php
@@ -3,28 +3,31 @@
 namespace AppBundle\Controller\Admin;
 
 use AppBundle\Entity\Coworker;
+use AppBundle\Service\NotificationService;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
- * Class CoworkerAdminController
+ * Class CoworkerAdminController.
  *
  * @category Controller
- * @package  AppBundle\Controller\Admin
+ *
  * @author   David Romaní <david@flux.cat>
  */
 class CoworkerAdminController extends BaseAdminController
 {
     /**
-     * Custom show action redirect to public frontend view
+     * Custom show action redirect to public frontend view.
      *
      * @param int|string|null $id
      * @param Request         $request
      *
      * @return Response
-     * @throws NotFoundHttpException If the object does not exist
+     *
+     * @throws NotFoundHttpException     If the object does not exist
      * @throws AccessDeniedHttpException If access is not granted
      */
     public function showAction($id = null, Request $request = null)
@@ -42,8 +45,38 @@ class CoworkerAdminController extends BaseAdminController
         return $this->redirectToRoute(
             'front_coworker_detail',
             array(
-                'slug'  => $object->getSlug(),
+                'slug' => $object->getSlug(),
             )
         );
+    }
+
+    /**
+     * Custom data action send email notification to fill the coworkers own data.
+     *
+     * @param int|string|null $id
+     * @param Request         $request
+     *
+     * @return RedirectResponse
+     *
+     * @throws NotFoundHttpException     If the object does not exist
+     * @throws AccessDeniedHttpException If access is not granted
+     */
+    public function dataAction($id = null, Request $request = null)
+    {
+        $request = $this->resolveRequest($request);
+        $id = $request->get($this->admin->getIdParameter());
+
+        /** @var Coworker $object */
+        $object = $this->admin->getObject($id);
+
+        if (!$object) {
+            throw $this->createNotFoundException(sprintf('unable to find the object with id : %s', $id));
+        }
+
+        /** @var NotificationService $messenger */
+        $messenger = $this->get('app.notification');
+        $messenger->sendCoworkerDataFormNotification($object);
+
+        return $this->redirectToRoute('admin_app_coworker_list');
     }
 }
