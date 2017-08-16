17edfdcba1d9a569916fa1cc756baba1e133f894
David Romaní <david@flux.cat>
2017-08-16 18:46:28 +0200

    enable recaptcha again

--- src/AppBundle/Form/Type/ContactNewsletterType.php
+++ src/AppBundle/Form/Type/ContactNewsletterType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form\Type;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -97,5 +98,17 @@ class ContactNewsletterType extends AbstractType
     public function getBlockPrefix()
     {
         return 'contact_newsletter';
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'AppBundle\Entity\ContactMessage',
+            )
+        );
     }
 }
