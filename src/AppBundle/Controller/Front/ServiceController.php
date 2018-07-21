4b9e893417b86069d87c98873e6a65344c4e72d4
David Romaní <david@flux.cat>
2018-07-21 19:57:04 +0200

    improve frontend service content 2

--- src/AppBundle/Controller/Front/ServiceController.php
+++ src/AppBundle/Controller/Front/ServiceController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller\Front;
 
+use AppBundle\Entity\ContactMessage;
+use AppBundle\Form\Type\ContactHomepageType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -16,10 +19,18 @@ class ServiceController extends Controller
     /**
      * @Route("/serveis", name="front_services")
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function servicesAction()
+    public function servicesAction(Request $request)
     {
-        return $this->render(':Frontend:Service/services.html.twig');
+        $contact = new ContactMessage();
+        $form = $this->createForm(ContactHomepageType::class, $contact);
+        $form->handleRequest($request);
+
+        return $this->render(':Frontend:Service/services.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
