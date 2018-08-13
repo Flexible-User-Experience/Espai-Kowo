9fcea1d529cffc09436764bb7493ea1a8e8f6702
David Romaní <david@flux.cat>
2018-08-13 17:45:45 +0200

    add Invoice & InvoiceLine entities

---
+++ src/AppBundle/Repository/InvoiceLineRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class InvoiceLineRepository.
+ *
+ * @category Repository
+ */
+class InvoiceLineRepository extends EntityRepository
+{
+}
---
+++ src/AppBundle/Repository/InvoiceRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class InvoiceRepository.
+ *
+ * @category Repository
+ */
+class InvoiceRepository extends EntityRepository
+{
+}
