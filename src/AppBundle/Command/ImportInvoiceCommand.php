4a794505c1634434397ec868b773a73c3c4aabef
David Romaní <david@flux.cat>
2018-09-05 17:06:44 +0200

    add ImportInvoiceCommand

---
+++ src/AppBundle/Command/ImportInvoiceCommand.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace AppBundle\Command;
+
+use PhpOffice\PhpSpreadsheet\Reader\Exception as ReaderException;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ImportInvoiceCommand.
+ *
+ * @category Command
+ */
+class ImportInvoiceCommand extends BaseCommand
+{
+    /**
+     * Configure command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('app:import:invoice')
+            ->setDescription('Import an invoice from XLS file')
+            ->addArgument(
+                'filepath',
+                InputArgument::REQUIRED,
+                'The XLS file path to import in database'
+            )
+            ->addOption(
+                'force',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the task will persist invoices into database'
+            )
+        ;
+    }
+
+    /**
+     * Execute command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Welcome
+        $this->setUp();
+        $this->printBeginCommand($output);
+        if ($input->getOption('force')) {
+            $output->writeln('<comment>--force option enabled (this option persists invoices into database)</comment>');
+        }
+
+        // Validate arguments
+        $filename = $input->getArgument('filepath');
+        if (!$this->fs->exists($filename)) {
+            throw new \InvalidArgumentException('The file '.$filename.' does not exist');
+        }
+
+        // Main loop
+        $output->writeln('Loading data, please wait...');
+        try {
+            // load file
+            $spreadsheet = $this->ss->loadWorksheetsXlsSpreadsheetReadOnly($filename, array('Lineas_Facturas_Emitidas', 'Facturas_Emitidas'));
+            // intialize counters
+            $dtStart = new \DateTime();
+            $index = 0;
+            $created = 0;
+            $worksheetIterator = $spreadsheet->getWorksheetIterator();
+            /** @var Worksheet $worksheet */
+            foreach ($worksheetIterator as $worksheet) {
+                $output->writeln($worksheet->getTitle());
+            }
+        } catch (ReaderException $e) {
+            $output->writeln('<error>XLS Reader Excetion: '.$e->getMessage().'</error>');
+        } catch (\Exception $e) {
+            $output->writeln('<error>Excetion: '.$e->getMessage().'</error>');
+        }
+
+        // EOF
+        $this->printEndCommand($output);
+    }
+}
--- src/AppBundle/Command/SendBirthdayNotificationCommand.php
+++ src/AppBundle/Command/SendBirthdayNotificationCommand.php
@@ -3,9 +3,6 @@
 namespace AppBundle\Command;
 
 use AppBundle\Entity\Coworker;
-use Doctrine\ORM\EntityManager;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,17 +12,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @category Command
  */
-class SendBirthdayNotificationCommand extends ContainerAwareCommand
+class SendBirthdayNotificationCommand extends BaseCommand
 {
-    /**
-     * @var EntityManager
-     */
-    protected $em;
-
-    /**
-     * Methods
-     */
-
     /**
      * Configure command
      */
@@ -45,7 +33,7 @@ class SendBirthdayNotificationCommand extends ContainerAwareCommand
     /**
      * Execute command
      *
-     * @param InputInterface $input
+     * @param InputInterface  $input
      * @param OutputInterface $output
      *
      * @return int|null|bool|void
@@ -57,46 +45,39 @@ class SendBirthdayNotificationCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        /** @var Router $router */
-        $router = $this->getContainer()->get('router');
-        $router->getContext()->setHost($this->getContainer()->getParameter('mailer_url_base'));
+        $this->setUp();
+        $this->rs->getContext()->setHost($this->getContainer()->getParameter('mailer_url_base'));
+        $currentDate = new \DateTime();
 
-        $output->writeln('<info>Welcome to send a notification command</info>');
-
+        $this->printBeginCommand($output);
         if ($input->getOption('delivery') === true) {
             $output->writeln(
                 '<comment>--delivery option enabled (the notifications will be sent by mail)</comment>'
             );
         }
 
-        // Command Vars
-        $this->em   = $this->getContainer()->get('doctrine.orm.default_entity_manager');
-        $currentDate = new \DateTime();
-        $messenger = $this->getContainer()->get('app.notification');
-
+        // Coworker notification to send current day
         $coworkersBirthday = $this->em->getRepository('AppBundle:Coworker')->getAllCoworkersBirthdayByDayAndMonth($currentDate->format('j'), $currentDate->format('n'));
-
         /** @var Coworker $coworker */
         foreach ($coworkersBirthday as $coworker) {
-            $output->writeln('Aniversari del/la coworker: ' . $coworker->getName() . ' ' . $coworker->getSurname());
+            $output->writeln('Aniversari del/la coworker: '.$coworker->getName().' '.$coworker->getSurname());
             if ($input->getOption('delivery') === true) {
-                $messenger->sendCoworkerBirthdayNotification($coworker);
+                $this->ns->sendCoworkerBirthdayNotification($coworker);
             }
         }
 
+        // Admin notification to send the day before
         $dateInterval = new \DateInterval('P1D');
         $dayBefore = $currentDate->add($dateInterval);
-
         $coworkersDayBeforeBirthday = $this->em->getRepository('AppBundle:Coworker')->getAllCoworkersBirthdayByDayAndMonth($dayBefore->format('j'), $dayBefore->format('n'));
-
         /** @var Coworker $coworker */
         foreach ($coworkersDayBeforeBirthday as $coworker) {
-            $output->writeln('Demà és l\'aniversari del/la coworker: ' . $coworker->getName() . ' ' . $coworker->getSurname());
+            $output->writeln('Demà és l\'aniversari del/la coworker: '.$coworker->getName().' '.$coworker->getSurname());
             if ($input->getOption('delivery') === true) {
-                $messenger->sendAdminBirthdayNotification($coworker);
+                $this->ns->sendAdminBirthdayNotification($coworker);
             }
         }
 
-        $output->writeln('<comment>END OF FILE.</comment>');
+        $this->printEndCommand($output);
     }
 }
