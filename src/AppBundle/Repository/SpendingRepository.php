97ea4d2779b2b1452ed3cb95a8e4bf0e58f581c1
David Romaní <david@flux.cat>
2018-09-06 18:51:58 +0200

    add Spending entity

---
+++ src/AppBundle/Repository/SpendingRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class SpendingRepository.
+ *
+ * @category Repository
+ */
+class SpendingRepository extends EntityRepository
+{
+}
