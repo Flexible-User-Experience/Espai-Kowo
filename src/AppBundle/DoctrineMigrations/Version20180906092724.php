54a0bd64a9ee0621172b21d026d3e924ef73ec85
David Romaní <david@flux.cat>
2018-09-06 11:28:00 +0200

    add SEPA xml recording migration

---
+++ src/AppBundle/DoctrineMigrations/Version20180906092724.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180906092724 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE invoice ADD is_sepa_xml_generated TINYINT(1) DEFAULT NULL, ADD sepa_xml_generation_date DATE DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE invoice DROP is_sepa_xml_generated, DROP sepa_xml_generation_date');
+    }
+}
