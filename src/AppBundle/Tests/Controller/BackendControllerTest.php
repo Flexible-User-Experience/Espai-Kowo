0818c26572ed60b92f65ff765288c9e8e1f6dd49
David Romaní <david@flux.cat>
2018-09-07 13:13:26 +0200

    refactoring admin menus & route namings 5

--- src/AppBundle/Tests/Controller/BackendControllerTest.php
+++ src/AppBundle/Tests/Controller/BackendControllerTest.php
@@ -47,27 +47,10 @@ class BackendControllerTest extends AbstractBaseTest
     {
         return array(
             array('/admin/dashboard'),
-            array('/admin/contacts/message/list'),
-            array('/admin/contacts/message/1/show'),
-            array('/admin/contacts/message/1/answer'),
-            array('/admin/contacts/message/1/delete'),
-            array('/admin/blog/tag/list'),
-            array('/admin/blog/tag/create'),
-            array('/admin/blog/tag/1/show'),
-            array('/admin/blog/tag/1/edit'),
-            array('/admin/blog/tag/1/delete'),
-            array('/admin/blog/post/list'),
-            array('/admin/blog/post/create'),
-            array('/admin/blog/post/1/edit'),
-            array('/admin/blog/post/1/delete'),
-            array('/admin/activities/activity/list'),
-            array('/admin/activities/activity/create'),
-            array('/admin/activities/activity/1/delete'),
-            array('/admin/activities/activity/1/edit'),
-            array('/admin/activities/category/list'),
-            array('/admin/activities/category/create'),
-            array('/admin/activities/category/1/delete'),
-            array('/admin/activities/category/1/edit'),
+            // Coworkers
+            array('/admin/coworkers/coworker/list'),
+            array('/admin/coworkers/coworker/create'),
+            array('/admin/coworkers/coworker/1/edit'),
             array('/admin/coworkers/category/list'),
             array('/admin/coworkers/category/create'),
             array('/admin/coworkers/category/1/delete'),
@@ -80,15 +63,7 @@ class BackendControllerTest extends AbstractBaseTest
             array('/admin/coworkers/social-network/create'),
             array('/admin/coworkers/social-network/1/delete'),
             array('/admin/coworkers/social-network/1/edit'),
-            array('/admin/coworkers/coworker/list'),
-            array('/admin/coworkers/coworker/create'),
-            array('/admin/coworkers/coworker/1/edit'),
-            array('/admin/configurations/province/list'),
-            array('/admin/configurations/province/create'),
-            array('/admin/configurations/province/1/edit'),
-            array('/admin/configurations/city/list'),
-            array('/admin/configurations/city/create'),
-            array('/admin/configurations/city/1/edit'),
+            // Sales
             array('/admin/sales/customer/list'),
             array('/admin/sales/customer/create'),
             array('/admin/sales/customer/1/edit'),
@@ -99,17 +74,50 @@ class BackendControllerTest extends AbstractBaseTest
             array('/admin/sales/invoice-line/create'),
             array('/admin/sales/invoice-line/1/edit'),
             array('/admin/sales/invoice-line/1/delete'),
+            // Purchases
+            array('/admin/purchases/provider/list'),
+            array('/admin/purchases/provider/create'),
+            array('/admin/purchases/provider/1/edit'),
             array('/admin/purchases/spending-category/list'),
             array('/admin/purchases/spending-category/create'),
             array('/admin/purchases/spending-category/1/edit'),
             array('/admin/purchases/spending-category/1/delete'),
-            array('/admin/purchases/provider/list'),
-            array('/admin/purchases/provider/create'),
-            array('/admin/purchases/provider/1/edit'),
+            // Blog
+            array('/admin/blog/post/list'),
+            array('/admin/blog/post/create'),
+            array('/admin/blog/post/1/edit'),
+            array('/admin/blog/post/1/delete'),
+            array('/admin/blog/tag/list'),
+            array('/admin/blog/tag/create'),
+            array('/admin/blog/tag/1/show'),
+            array('/admin/blog/tag/1/edit'),
+            array('/admin/blog/tag/1/delete'),
+            // Activities
+            array('/admin/activities/activity/list'),
+            array('/admin/activities/activity/create'),
+            array('/admin/activities/activity/1/delete'),
+            array('/admin/activities/activity/1/edit'),
+            array('/admin/activities/category/list'),
+            array('/admin/activities/category/create'),
+            array('/admin/activities/category/1/delete'),
+            array('/admin/activities/category/1/edit'),
+            array('/admin/activities/category/1/show'),
+            // Contacts
+            array('/admin/contacts/message/list'),
+            array('/admin/contacts/message/1/show'),
+            array('/admin/contacts/message/1/answer'),
+            array('/admin/contacts/message/1/delete'),
+            // Configurations
             array('/admin/configurations/user/list'),
             array('/admin/configurations/user/create'),
             array('/admin/configurations/user/1/edit'),
             array('/admin/configurations/user/1/delete'),
+            array('/admin/configurations/province/list'),
+            array('/admin/configurations/province/create'),
+            array('/admin/configurations/province/1/edit'),
+            array('/admin/configurations/city/list'),
+            array('/admin/configurations/city/create'),
+            array('/admin/configurations/city/1/edit'),
         );
     }
 
@@ -135,22 +143,13 @@ class BackendControllerTest extends AbstractBaseTest
     public function provideNotFoundUrls()
     {
         return array(
-            array('/admin/blog/tag/batch'),
-            array('/admin/blog/post/batch'),
-            array('/admin/contacts/message/create'),
-            array('/admin/contacts/message/1/edit'),
-            array('/admin/contacts/message/batch'),
+            // Coworkers
+            array('/admin/coworkers/coworker/batch'),
+            array('/admin/coworkers/coworker/1/delete'),
             array('/admin/coworkers/category/batch'),
             array('/admin/coworkers/social-network-category/batch'),
             array('/admin/coworkers/social-network/batch'),
-            array('/admin/coworkers/coworker/batch'),
-            array('/admin/coworkers/coworker/1/delete'),
-            array('/admin/configurations/province/batch'),
-            array('/admin/configurations/province/1/show'),
-            array('/admin/configurations/province/1/delete'),
-            array('/admin/configurations/city/batch'),
-            array('/admin/configurations/city/1/show'),
-            array('/admin/configurations/city/1/delete'),
+            // Sales
             array('/admin/sales/customer/batch'),
             array('/admin/sales/customer/1/show'),
             array('/admin/sales/customer/1/delete'),
@@ -159,15 +158,31 @@ class BackendControllerTest extends AbstractBaseTest
             array('/admin/sales/invoice/1/delete'),
             array('/admin/sales/invoice-line/batch'),
             array('/admin/sales/invoice-line/1/show'),
-            array('/admin/purchases/spending-category/batch'),
-            array('/admin/purchases/spending-category/1/show'),
+            // Purchases
             array('/admin/purchases/provider/batch'),
             array('/admin/purchases/provider/1/show'),
             array('/admin/purchases/provider/1/delete'),
+            array('/admin/purchases/spending-category/batch'),
+            array('/admin/purchases/spending-category/1/show'),
+            // Blog
+            array('/admin/blog/post/batch'),
+            array('/admin/blog/tag/batch'),
+            // Activities
             array('/admin/activities/activity/batch'),
             array('/admin/activities/category/batch'),
+            // Contacts
+            array('/admin/contacts/message/create'),
+            array('/admin/contacts/message/1/edit'),
+            array('/admin/contacts/message/batch'),
+            // Configurations
             array('/admin/configurations/user/show'),
             array('/admin/configurations/user/batch'),
+            array('/admin/configurations/province/batch'),
+            array('/admin/configurations/province/1/show'),
+            array('/admin/configurations/province/1/delete'),
+            array('/admin/configurations/city/batch'),
+            array('/admin/configurations/city/1/show'),
+            array('/admin/configurations/city/1/delete'),
         );
     }
 
@@ -194,8 +209,7 @@ class BackendControllerTest extends AbstractBaseTest
     {
         return array(
             array('/admin/coworkers/coworker/1/show'),
-            array('/admin/activitats/activitat/1/show'),
-            array('/admin/activities/category/1/show'),
+            array('/admin/activities/activity/1/show'),
             array('/admin/blog/post/1/show'),
         );
     }
