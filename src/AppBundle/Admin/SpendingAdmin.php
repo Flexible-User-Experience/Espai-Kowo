a7aff1bc1db19f7c827b7132eab406719ac621e9
David Romaní <david@flux.cat>
2018-09-07 18:31:50 +0200

    refactor SpendingCategory admin 1

--- src/AppBundle/Admin/SpendingAdmin.php
+++ src/AppBundle/Admin/SpendingAdmin.php
@@ -2,10 +2,16 @@
 
 namespace AppBundle\Admin;
 
+use AppBundle\Entity\Provider;
+use AppBundle\Entity\SpendingCategory;
+use AppBundle\Enum\PaymentMethodEnum;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\CoreBundle\Form\Type\DatePickerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 /**
  * Class SpendingAdmin.
@@ -29,20 +35,76 @@ class SpendingAdmin extends AbstractBaseAdmin
         $formMapper
             ->with('backend.admin.general', $this->getFormMdSuccessBoxArray(5))
             ->add(
+                'date',
+                DatePickerType::class,
+                array(
+                    'label' => 'backend.admin.spending.date',
+                    'format' => 'd/M/y',
+                    'required' => true,
+
+                )
+            )
+            ->add(
+                'category',
+                EntityType::class,
+                array(
+                    'label' => 'backend.admin.spending.category',
+                    'required' => false,
+                    'class' => SpendingCategory::class,
+                    'query_builder' => $this->rm->getSpendingCategoryRepository()->getEnabledSortedByNameQB(),
+                )
+            )
+            ->add(
+                'provider',
+                EntityType::class,
+                array(
+                    'label' => 'backend.admin.spending.provider',
+                    'required' => true,
+                    'class' => Provider::class,
+                    'query_builder' => $this->rm->getProviderRepository()->getEnabledSortedByNameQB(),
+                )
+            )
+            ->add(
                 'description',
                 null,
                 array(
                     'label' => 'backend.admin.spending.description',
+                    'required' => false,
                 )
             )
             ->end()
             ->with('backend.admin.controls', $this->getFormMdSuccessBoxArray(3))
             ->add(
-                'enabled',
+                'baseAmount',
+                null,
+                array(
+                    'label' => 'backend.admin.invoice.baseAmount',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'isPayed',
                 CheckboxType::class,
                 array(
-                    'label' => 'backend.admin.enabled',
+                    'label' => 'backend.admin.invoice.isPayed',
                     'required' => false,
+                )
+            )
+            ->add(
+                'paymentDate',
+                DatePickerType::class,
+                array(
+                    'label' => 'backend.admin.invoice.paymentDate',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'paymentMethod',
+                ChoiceType::class,
+                array(
+                    'label' => 'backend.admin.customer.payment_method',
+                    'choices' => PaymentMethodEnum::getEnumArray(),
+                    'required' => true,
                 )
             )
             ->end();
--- src/AppBundle/Repository/SpendingCategoryRepository.php
+++ src/AppBundle/Repository/SpendingCategoryRepository.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Query;
 
 /**
  * Class SpendingCategoryRepository.
@@ -11,4 +13,31 @@ use Doctrine\ORM\EntityRepository;
  */
 class SpendingCategoryRepository extends EntityRepository
 {
+    /**
+     * @return QueryBuilder
+     */
+    public function getEnabledSortedByNameQB()
+    {
+        return $this->createQueryBuilder('sc')
+            ->where('sc.enabled = :enabled')
+            ->setParameter('enabled', true)
+            ->orderBy('sc.name', 'ASC')
+            ;
+    }
+
+    /**
+     * @return Query
+     */
+    public function getEnabledSortedByNameQ()
+    {
+        return $this->getEnabledSortedByNameQB()->getQuery();
+    }
+
+    /**
+     * @return array
+     */
+    public function getEnabledSortedByName()
+    {
+        return $this->getEnabledSortedByNameQ()->getResult();
+    }
 }
