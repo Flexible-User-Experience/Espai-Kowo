35c9ffb411a94c686545b687f1cd7f9d52179223
David Romaní <david@flux.cat>
2018-09-15 18:28:22 +0200

    improve file uploader in SpendingAdmin 2

--- src/AppBundle/Admin/SpendingAdmin.php
+++ src/AppBundle/Admin/SpendingAdmin.php
@@ -261,6 +261,7 @@ class SpendingAdmin extends AbstractBaseAdmin
                     'label' => 'backend.admin.actions',
                     'actions' => array(
                         'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'document' => array('template' => '::Admin/Buttons/list__action_spending_document_button.html.twig'),
                         'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
                     ),
                 )
